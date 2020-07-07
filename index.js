aad6355650baabae57e8ca54e8c8feeaa11359c2
ZYROUGE <me@zyrouge.gq>
2020-07-07 05:01:54 +0000

    Fixed error in url.js Changed js to json Some minor fixes

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const axios = require("axios");
-const urls = require("./url.js");
+const urls = require("./url.json");
+
+const time = () => (new Date().toLocaleTimeString(undefined, { timeZone: "Asia/Kolkata" }));
 
 const uptimerobo = async () => {
     setInterval(() => {
         urls.forEach(url => {
-            axios.get(url).then(() => console.log("Pong at " + Date.now())).catch(() => {});
+              axios.get(url).then(() => {
+                  console.log(`Pong ${url} at ${time()}`);
+              }).catch(() => {});
         });
     }, 60 * 1000);
 };
 
 uptimerobo();
+console.log(`Started Uptimerobo at ${time()}`);
