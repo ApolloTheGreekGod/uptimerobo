5d4b092c643b33670489d1623fae6225a00b087e
ZYROUGE <me@zyrouge.gq>
2020-11-19 11:53:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-const axios = require("axios");
-const urls = require("./url.json");
+const axios = require("axios").default;
+const fs = require("fs");
+const urls = fs.readFileSync("./url.txt").toString().split("\n");
 
 const time = () => (new Date().toLocaleTimeString(undefined, { timeZone: "Asia/Kolkata" }));
 
 const uptimerobo = async () => {
     setInterval(() => {
         urls.forEach(url => {
-              axios.get(url).then(() => {
-                  console.log(`Pong ${url} at ${time()}`);
-              }).catch(() => {});
+            axios.get(url).then(() => {
+                console.log(`Pong ${url} at ${time()}`);
+            }).catch(() => { });
         });
     }, 60 * 1000);
 };
